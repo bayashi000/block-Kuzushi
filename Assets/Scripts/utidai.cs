6a21ed47619e8065f2454c2f2272cab8d4ba1609
bayashi000 <bayashiko1020@yahoo.co.jp>
2019-02-20 02:37:44 +0900

    ボール移動できた

--- Assets/Scripts/utidai.cs
+++ Assets/Scripts/utidai.cs
@@ -2,44 +2,17 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-
-
 public class utidai : MonoBehaviour
 {
-    //この中で使うものを宣言していく
-
-    public float speed = 3;
-
-
-
-    // 画面生成時に一度だけ処理するのをカッコ内にかく
+    // Start is called before the first frame update
     void Start()
     {
         
     }
 
-    // ゲーム中ずっと処理するのをカッコ内にかく
+    // Update is called once per frame
     void Update()
     {
-        if(Input.GetKey("left"))
-
-        {
-            this.transform.position -= new Vector3(speed * Time.deltaTime, 0, 0);
-        }
-        if (Input.GetKey("right"))
-        {
-            this.transform.position += new Vector3( speed * Time.deltaTime,0,0);
-        }
-        if(Input.GetKey("down"))
-        {
-            this.transform.position -= new Vector3(0,0, speed * Time.deltaTime);
-        }
-        if(Input.GetKey("up"))
-        {
-            this.transform.position += new Vector3(0, 0, speed* Time.deltaTime);
-
-         }
-
-    }   
-      
+        
+    }
 }
