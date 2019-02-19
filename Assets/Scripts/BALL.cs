d826957936a43e7a75c69745389e207a9ca1c30b
bayashi000 <bayashiko1020@yahoo.co.jp>
2019-02-20 02:00:29 +0900

    中間地点

---
+++ Assets/Scripts/BALL.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Collections;
+
+
+public class BALL : MonoBehaviour{
+
+    private float speed = 20.0f;    //これを追加
+
+    
+
+     // Start is called before the first frame update
+    void Start()
+    {
+                this.GetComponent<Rigidbody>().AddForce(
+                  (transform.forward + transform.right) * speed,
+                   ForceMode.VelocityChange);
+
+            }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        //衝突判定
+        if (collision.gameObject.tag == "block")
+        {
+            //相手のタグがBallならば、自分を消す
+            Destroy(collision.gameObject);
+        }
+    }
+
+}
---
+++ Assets/Scripts/utidai.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class utidai : MonoBehaviour
+{
+    //この中で使うものを宣言していく
+
+    public float speed = 3;
+
+
+
+    // 画面生成時に一度だけ処理するのをカッコ内にかく
+    void Start()
+    {
+        
+    }
+
+    // ゲーム中ずっと処理するのをカッコ内にかく
+    void Update()
+    {
+        if(Input.GetKey("left"))
+
+        {
+            this.transform.position -= new Vector3(speed * Time.deltaTime, 0, 0);
+        }
+        if (Input.GetKey("right"))
+        {
+            this.transform.position += new Vector3( speed * Time.deltaTime,0,0);
+        }
+        if(Input.GetKey("down"))
+        {
+            this.transform.position -= new Vector3(0,0, speed * Time.deltaTime);
+        }
+        if(Input.GetKey("up"))
+        {
+            this.transform.position += new Vector3(0, 0, speed* Time.deltaTime);
+
+         }
+
+    }   
+      
+}
